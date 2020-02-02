6d67547d65ca5346b29f073989ddb7d7c70cb908
Emmanuel Bairaktaris <bmanos@users.noreply.github.com>
2020-02-02 13:56:18 +0200

    Create brew.sh

---
+++ brew.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# Make script executabe first: chmod 755 brew.sh
+# the run it: ./brew.sh
+
+# Install Brew Packages
+brew install python
+#brew install cocoapods
+brew install micro
+brew install mongodb-community@4.2
+brew install nmap
+brew install node
+brew install yarn
+brew install wireshark
+
+# Install MacOS Applications
+#brew cask install acorn cracked
+brew cask install angry-ip-scanner
+brew cask install appcleaner
+brew cask install arduino
+brew cask install autodesk-fusion360
+brew cask install balenaetcher
+#battery health 2 - App Store
+brew cask install brackets
+brew cask install ccleaner
+brew cask install db-browser-for-sqlite
+brew cask install dropbox
+#eagle 7
+brew cask install eagle
+#brew cask install fanny
+#filezilla - https://filezilla-project.org/download.php?type=client
+brew cask install handbrake
+brew cask install fritzing
+brew cask install firefox
+brew cask install franz
+#github desktop - https://desktop.github.com
+brew cask install google-chrome
+brew cask install impactor
+brew cask install iterm2
+#brew cask install istat-menus cracked
+#Java from www.java.com
+brew cask install knockkcnock
+#makecode microbit - https://microbit.org/guide/quick/
+#microsoft-office iso file
+#microsoft remote desktop - App Store
+brew cask install mongodb-compass-community
+brew cask install mu-editor
+brew cask install onedrive
+#on screen control lg
+brew cask install owncloud
+brew cask install postman
+brew cask install qbittorrent
+brew cask install sdformatter
+#startup disk full fixer - App Store
+brew cask install superduper
+#brew cask install the-unarchiver - App Store
+brew cask install tor-browser
+#tightvnc - https://www.tightvnc.com/download.php
+brew cask install ultimaker-cura
+brew cask install visual-studio-code
+brew cask install vlc
+brew cask install vnc-viewer
+#xcode - App Store
+#xcode-beta - https://developer.apple.com
+#adobe flash player - https://get.adobe.com/flashplayer/npapiosx/
+
+# Install Source Code Pro Font
+#brew tap homebrew/cask-fonts
+#brew cask install font-source-code-pro
+
+# Install Nerd Fonts
+brew tap homebrew/cask-fonts && brew cask install font-Cousine-nerd-font
